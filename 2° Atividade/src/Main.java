2f7ef8f1fc6ec049c43e2951c48449f7fc6429a6
renezoka <rmmedeiros300@gmail.com>
2025-02-24 00:07:06 -0300

    Update Main.java

--- 2° Atividade/src/Main.java
+++ 2° Atividade/src/Main.java
@@ -4,6 +4,9 @@ pedido1.finalizarPedido();
 
 Pedido pedido2 = new Pedido(200.0, "boleto");
 pedido2.finalizarPedido();
+
+Pedido pedido3 = new Pedido(56.0, "pix");
+pedido3.finalizarPedido();
 }
 
 }
